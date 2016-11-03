a26e7a2776ad5a94bb581c6f6d2d850df7fc0c84
Jamie Schouten <jamiecorsica@hotmail.com>
2016-11-03 17:10:41 +0100

    Added some migrations
    Colloquium_users
    Colloquium_examitors
    Theme_users
    Themes
    Colloquium_theme

---
+++ database/migrations/2016_11_03_160050_create_theme_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThemeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('theme_users', function (Blueprint $table) {
+            $table->integer('theme_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            $table->primary(['theme_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('theme_users');
+    }
+}

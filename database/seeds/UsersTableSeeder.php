1651f5b4afde4d649fe3e96e882999a405bb004b
Sander van Kasteel <info@sandervankasteel.nl>
2016-11-04 19:18:50 +0100

    Added a default Admin account for the seeders! email is admin@colloquium.app and password is 123123123

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        // Create a default  "admin" account!
+        \App\Models\User::create([
+            'first_name' => 'Admin',
+            'insertion' => '',
+            'last_name' => 'istrator',
+            'email' => 'admin@colloquium.app',
+            'password' => bcrypt('123123123'),
+            'verified' => '1',
+            'role_id' => '4',
+            'enabled' => '1',
+            'prefered_language' => 'nl',
+            'image' => ''
+        ]);
+
         factory(App\User::class, 10)->create();
     }
 }

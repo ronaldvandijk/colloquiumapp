6468ac39f82972ff739c1d1bc0cf683cd9479663
Timothy Ravor <timothyravor@live.nl>
2016-11-06 16:08:18 +0100

    Get the column names from the selected table

---
+++ app/BaseClass.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseClass extends Model
+{
+    public function getproperties($table)
+    {
+        $properties = array();
+
+        $columnNames = Schema::getColumnListing('$table');
+
+        foreach ($columnNames as $property)
+        {
+            array_push($properties, $property);
+        }
+
+        //Return the properties of the selected table.
+        return $properties;
+    }
+
+}

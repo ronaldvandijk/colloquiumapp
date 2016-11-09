e9a41fb5bf04145fc408194ca366963cf587e047
IceTimux <icetimux@gmail.com>
2016-11-09 21:09:17 +0100

    added comments to SearchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,28 +13,46 @@ class SearchController extends Controller
 {
     public function index()
     {
+        // Instantiate an array to store the properly sorted Colloquiums
         $colloquiumDates = array();
+
+        // Retrieve all the Colloquiums
         $colloquiums = Colloquium::all();
 
+        // For each colloquim, push its date formated as 'Y-m-d' as a key in the $colloquiumDates assosciative array
         foreach ($colloquiums as $colloquium) {
             $colloquiumDates[strval(date('Y-m-d', strtotime($colloquium->start_date)))] = [];
         }
 
+        // Go through the $colloquiumDates and retrieve all colloquiums with the same start_date as the array key
         foreach ($colloquiumDates as $date => $colloquium) {
             $colloquiums = DB::select('select colloquia.*, rooms.name from colloquia , rooms where colloquia.room_id = rooms.id AND DATE(colloquia.start_date) = ?', [$date]);
+            // pus the retrieved collquiums to the associated key in the $colloquiumDates array
             $colloquiumDates[$date] = $colloquiums;
         } 
 
+        // Sort the $colloquiumDates keys 
+        ksort($colloquiumDates);
+
+        // Return agenda index view
         return view('agenda.index', ['colloquiumDates' => $colloquiumDates]);
     }
 
     public function details($id)
     {
+        // Retrieve the colloquium with the matching $id
         $colloquium = Colloquium::find($id);
+
+        // Retrieve the user which matches the $colloquium->id
         $user = User::find($colloquium->user_id);
+
+        // Retrieve the colloquium type that matches the $colloquium->tyoe_id
         $type = ColloquiumType::find($colloquium->type_id);
+
+        // Retrieve the room that matches the $colloquium->room_id
         $room = Room::find($colloquium->room_id);
 
+        // return agenda details view
         return view('agenda.details', ['colloquium' => $colloquium, 'user' => $user, 'type' => $type, 'room' => $room]);
     }
 }

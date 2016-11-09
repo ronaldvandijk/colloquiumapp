16c8295093472a91163debe02ef7d07ad39443ae
Melle Dijkstra <melle210202@gmail.com>
2016-11-09 12:29:13 +0100

    Begin Colloquia overview in HomeController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+    <div class="container">
+        <h2>({{ count($colloquia) }}) Colloquia gevonden op deze locatie</h2>
+        <div class="row">
+            @foreach($colloquia as $colloquium)
+                <div class="col-sm-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">{{ $colloquium->title }}</div>
+                        <div class="panel-body">{{ $colloquium->description }}</div>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
-</div>
 @endsection

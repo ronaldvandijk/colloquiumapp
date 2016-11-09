82649473bddb6abb9d783a19c926d6b4d1a61281
IceTimux <icetimux@gmail.com>
2016-11-09 20:48:08 +0100

    made modal view pretty

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -6,16 +6,11 @@
             <div class="panel panel-default">
                 <div class="panel-body schedule-item" data-toggle="modal" data-target="#colloquiumDate-{{$date}}">
                     <div class="row">
-                        <div class="col-xs-4">
-                            <div class="well">
-                                <h3>{{strtoupper(date('D', strtotime($date)))}}</h3>
-                            </div>
+                        <div class="col-xs-2    ">
+                            <h4><b>{{date('D', strtotime($date))}}</b></h4>
                         </div>
-                        <div class="col-xs-8">
+                        <div class="col-xs-10">
                             <h4>{{date('j M Y', strtotime($date))}} <span class="label label-default pull-right">{{count($colloquiums)}} Events</span></h4>
-                            @foreach($colloquiums as $colloquium)
-                                <p>{{$colloquium->title}}</p>
-                            @endforeach
                         </div>
                     </div>
                 </div>
@@ -29,16 +24,21 @@
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">{{$date}}</h4>
+                <h4 class="modal-title" id="myModalLabel">{{date('d M Y', strtotime($date))}}</h4>
               </div>
               <div class="modal-body">
                 @foreach ($colloquiums as $colloquium)
                     <div class="panel panel-default">
                         <div class="panel-body">
-                            <h4><b>{{$colloquium->title}}</b> <p class="pull-right">{{$colloquium->name}}</p></h4>
+                            <h4><b>{{$colloquium->title}}</b> <span class="pull-right"></span></h4>
                             <div class="deets">
-                                <p>{{date('H:i', strtotime($colloquium->start_date))}} - {{date('H:i', strtotime($colloquium->end_date))}}</p>
-                                <a href="agenda/details/{{$colloquium->id}}" class="btn btn-sm btn-primary pull-right">Details</a>
+                                <p>
+                                    <b>Room: </b>{{$colloquium->name}}
+                                    <br>
+                                    <b>Time: </b>{{date('H:i', strtotime($colloquium->start_date))}} - {{date('H:i', strtotime($colloquium->end_date))}}
+                                    <a href="agenda/details/{{$colloquium->id}}" class="btn btn-sm btn-primary pull-right">Details</a>
+                                </p>
+                                
                             </div>
                         </div>
                     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,7 @@
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ url('/agenda') }}">Agenda</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>

4636c1fd49ed240fcc74914b7d249b41f0df5d19
Maarten Oosting <maarten.tutoworld@gmail.com>
2016-11-09 12:23:47 +0100

    Add IDE-helper files

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,6 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Location
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $city_id
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Building[] $buildings
+ * @property-read \App\Models\City $city
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Location whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Location whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Location whereCityId($value)
+ * @mixin \Eloquent
+ */
 class Location extends Model
 {
     public $timestamps = false;

149a8d33cf40ceebcefccde260ea6e33ce90c47f
Sander van Kasteel <info@sandervankasteel.nl>
2016-11-10 00:43:43 +0100

    Expended the base.edit a bit

--- resources/views/base/edit.blade.php
+++ resources/views/base/edit.blade.php
@@ -5,17 +5,32 @@
         <h2>Base edit</h2>
         <div class="col-md-10">
             <div class="panel panel-default">
-                <div class="panel-heading">
-                    <b>Lorum ipsum</b>
-                </div>
+                <div class="panel-heading"><b>Something bewerken</b></div>
+
                 <div class="panel-body">
-                    <table>
-                        <thead>
-                            @foreach($properties as $property)
-                                {{$property}}
-                            @endforeach
-                        </thead>
-                    </table>
+                    <form method="post" action="{{ $baseUrl }}update">
+                        {{ method_field('PUT') }}
+                        {{ csrf_field() }}
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                @foreach($properties as $property)
+                                    @if($property != "id")
+                                        <div class="form-group">
+                                            <label>{{ $property }}</label>
+                                            <input type="text" class="form-control" value="{{ $data->$property }}" name="{{$property}}">
+                                        </div>
+                                    @else
+                                        <input type="hidden" value="{{ $data->$property }}" name="id">
+                                    @endif
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-success pull-right">Save</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>

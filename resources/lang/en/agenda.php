70090d7bbea42571d0fafb61aeea3fdd57eca3fd
IceTimux <icetimux@gmail.com>
2016-11-09 23:05:49 +0100

    populated en locale

--- resources/lang/en/agenda.php
+++ resources/lang/en/agenda.php
@@ -1,5 +1,16 @@
 <?php
     return [
-        
+        'events' => 'Events',
+        'room' => 'Room',
+        'time' => 'Time',
+        'details' => 'Details',
+        'back' => 'Terug',
+        'interested' => 'Interested',
+        'speaker' => 'Speaker',
+        'type' => 'Type',
+        'duration' => 'Duration',
+        'language' => 'Language',
+        'location' => 'Location',
+        'description' => 'Description'
     ];
 ?>

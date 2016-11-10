8dcbb9bf825b6925b99933c928bfb103308d1ece
Maarten Oosting <maarten.tutoworld@gmail.com>
2016-11-10 11:39:11 +0100

    route hotfix

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'role:administrator'], functi
     Route::get('user/edit/{user}', 'Admin\UsersController@edit');
     Route::post('user/update', 'Admin\UsersController@edit');
 
-    Route::get('{type}', 'Admin\BaseController@overview');
+//    Route::get('{type}', 'Admin\BaseController@overview');
     Route::get('{type}/create', 'Admin\BaseController@create');
     Route::post('{type}/store', 'Admin\BaseController@store');
     Route::get('{type}/edit/{id}', 'Admin\BaseController@update');

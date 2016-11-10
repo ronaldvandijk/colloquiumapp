7c40b8da1d91ac1399f65cf0dd32c3ffd31a6c85
Sander van Kasteel <sander@hotelchamp.com>
2016-11-10 17:08:12 +0100

    Added NL translations

---
+++ resources/lang/nl/user/colloquium/edit.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'title' => 'Titel',
+    'description' => 'Omschrijving',
+    'start_date' => 'Start datum',
+    'type' => 'Type',
+    'language' => 'Taal',
+    'status' => 'Status',
+    'company_image' => 'Afbeeling van het bedrijf',
+    'company_url' => 'Website van het bedrijf',
+];

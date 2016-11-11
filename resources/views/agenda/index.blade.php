f49cc311be0a7c2a929cdebb72724b6bb551c645
IceTimux <icetimux@gmail.com>
2016-11-11 12:30:36 +0100

    removed searchbar

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -2,37 +2,7 @@
 
 @section('content')
     <div class="container-fluid">
-        <form action="" method="POST">
-            <div class="input-group">
-                <select class="selectpicker" data-selected-text-format="count > 1" data-width="25%" Title="Klassen" name="Klassen" multiple>
-                    @foreach($users as $user)
-                        <option>{{ $user->present()->full_name }}</option>
-                    @endforeach
-                </select>
-                <select class="selectpicker" data-selected-text-format="count > 1" data-width="25%" Title="Locaties" name="Klassen" multiple>
-                    <!-- TODO: Locaties dynamisch vullen. -->
-                    <option>Loc1</option>
-                    <option>loc2</option>
-                    <option>loc3</option>
-                </select>
-
-                <!-- Date Picker -->
-                <div id="sandbox-container">
-                    <input type="text" placeholder="Datum" class="form-control">
-                </div>
-            </div>
-            <div class="input-group">
-
-                <input type="text"  class=" form-control " placeholder="Search for...">
-                <span class="input-group-btn">
-                    <button class="btn btn-default btn-primary" type="button"><i class="fa fa-search" aria-hidden="true"></i> Search</button>
-                </span>
-            </div>
-        </form>
-    <script> $('#sandbox-container input').datepicker({ });</script>
-
         @foreach($colloquiumCollection as $colloquiumCollectionDate => $colloquiums)
-
             <div class="panel panel-default">
                 <div class="panel-body schedule-item" data-toggle="modal" data-target="#colloquiumDate-{{ format('d-m-Y', $colloquiumCollectionDate) }}">
                     <div class="row">

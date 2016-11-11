9da114d4d2cd4212fb55073e052a85bea0e54748
Sam Lakerveld <dark@novaember.com>
2016-11-11 15:28:31 +0100

    Added MailtemplateTableSeeder

--- app/Models/Mailtemplate.php
+++ app/Models/Mailtemplate.php
@@ -4,7 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Mailtemplate
+ *
+ * @mixin \Eloquent
+ * @property integer $id
+ * @property string $name
+ * @property string $body
+ * @property string $subject
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Mailtemplate whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Mailtemplate whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Mailtemplate whereBody($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Mailtemplate whereSubject($value)
+ */
 class Mailtemplate extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'subject',
+        'body',
+    ];
 }

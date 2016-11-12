65cbeaccdb5fd6b4d33e882c23ae7f204f8eba6d
Sander van Kasteel <info@sandervankasteel.nl>
2016-11-12 20:05:10 +0100

    Added debugbar

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Package Service Providers...

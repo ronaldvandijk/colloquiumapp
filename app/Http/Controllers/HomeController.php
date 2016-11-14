542abf2f2922aa6f278e4b664d6a4134b860d537
Melle Dijkstra <melle210202@gmail.com>
2016-11-14 14:39:49 +0100

    Added TV View Backend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Colloquium;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -26,5 +25,30 @@ class HomeController extends Controller
         return view('home', [
             'colloquia' => Colloquium::all()
         ]);
+    }
+
+    /**
+     * This is the action which will display the TV screen with relevant colloquia for this location
+     * @param null $location The location to filter on
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function tv($location = null) {
+
+        $query = Colloquium::getQuery();
+
+        // If user filled in location then filter on it
+        if(!is_null($location)) {
+            $query->join('rooms', 'rooms.id', '=', 'colloquia.room_id')
+                ->join('buildings', 'buildings.id', '=', 'rooms.building_id')
+                ->join('locations', 'locations.id', '=', 'buildings.location_id')
+                ->where('location.name','=',$location);
+        };
+
+        // Get upcoming colloquia first
+        $query->whereDate('start_date', '>=', Carbon::now()->format('Y-m-d'));
+        $query->orderBy('start_date', 'asc');
+
+        // Return first 20 colloquia to view
+        return view('tv', ['colloquia' => $query->take(20)->get()]);
     }
 }

f51ded8b215539c3bc60b246a5aa5aae2feecfc2
Rens Santing <renssanting@gmail.com>
2016-11-14 12:05:42 +0100

    forgot to remove middleware from controller

--- app/Http/Controllers/Admin/BuildingController.php
+++ app/Http/Controllers/Admin/BuildingController.php
@@ -24,10 +24,6 @@ class BuildingController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Display a listing of the resource.

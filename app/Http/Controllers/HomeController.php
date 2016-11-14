438da449eddfff0b3a4066e62d565802cb0d6ec3
Jamie Schouten <jamiecorsica@hotmail.com>
2016-11-14 12:38:58 +0100

    Added where statement to agenda to show only items in the future

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
     public function index()
     {
         return view('home', [
-            'colloquia' => Colloquium::all()
+            'colloquia' => Colloquium::all(),
         ]);
     }
 }

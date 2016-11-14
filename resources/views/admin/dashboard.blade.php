3baabd87346af8301e88da857ee230c599615a80
Chistaen <cj_robert@outlook.com>
2016-11-14 23:42:44 +0100

    Merge conflict #2

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,26 +9,25 @@
 @section('content')
     <div id="admin-page" class="container">
         <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <a href="{{ action('Admin\LocationController@index') }}" ><div class="panel panel-default panel-admin"><i class="fa fa-globe"></i> {{ trans('admin/dashboard.locations') }}</div></a>
+            <div class="col-sm-6 col-md-4">
+                <a href="{{ action('Admin\BuildingController@index') }}" ><div class="panel panel-default panel-admin"><i class="fa fa-building"></i> {{ trans('admin/dashboard.buildings') }}</div></a>
             </div>
-            <div class="col-sm-12 col-md-6">
-                <a href="{{ action('Admin\UsersController@overview') }}" ><div class="panel panel-default panel-admin"><i class="fa fa-users"></i> {{ trans('admin/dashboard.users') }}</div></a>
+            <div class="col-sm-6 col-md-4">
+                <a href="{{ action('Admin\CityController@index') }}" ><div class="panel panel-default panel-admin"><i class="fa fa-globe"></i> {{ trans('admin/dashboard.cities') }}</div></a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a href="{{ action('Admin\LocationController@index') }}" ><div class="panel panel-default panel-admin"><i class="fa fa-map-pin"></i> {{ trans('admin/dashboard.locations') }}</div></a>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-12 col-md-6">
+            <div class="col-sm-6 col-md-4">
+                <a href="{{ action('Admin\RoomController@index') }}" ><div class="panel panel-default panel-admin"><i class="fa fa-square-o"></i> {{ trans('admin/dashboard.rooms') }}</div></a>
+            </div>
+            <div class="col-sm-6 col-md-4">
                 <a href="{{ action('Admin\ThemeController@index') }}" ><div class="panel panel-default panel-admin"><i class="fa fa-pencil-square"></i> {{ trans('admin/dashboard.themes') }}</div></a>
             </div>
-            <div class="col-sm-12 col-md-6">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div><i class="fa fa-rss"></i> {{ trans('admin/dashboard.activity_stream') }}</div>
-                    </div>
-                    <div class="panel-body">
-                         {{-- TODO: Activity Stream --}}
-                    </div>
-                </div>
+            <div class="col-sm-6 col-md-4">
+                <a href="{{ action('Admin\UsersController@overview') }}" ><div class="panel panel-default panel-admin"><i class="fa fa-users"></i> {{ trans('admin/dashboard.users') }}</div></a>
             </div>
         </div>
     </div>

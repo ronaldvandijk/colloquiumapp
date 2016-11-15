13bdab41a11a300ca52c14ebe9ab9db996909ed1
Sander van Kasteel <info@sandervankasteel.nl>
2016-11-15 23:09:14 +0100

    Added comments for the ColloquimTheme

--- app/Models/ColloquiumTheme.php
+++ app/Models/ColloquiumTheme.php
@@ -1,23 +1,46 @@
 <?php
+/**
+ * ColloquiumTheme
+ * @author       Sander van Kasteel <info@sandervankasteel.nl>
+ */
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class ColloquiumTheme
+ * @package App\Models
+ */
 class ColloquiumTheme extends Model
 {
 
+    /**
+     * A simple boolean to let Eloquent know if there are created_at and updated_at fields in the database
+     * @var bool
+     */
     public $timestamps = false;
 
+    /**
+     * A simple array to let eloquent know which fields can be updated
+     *
+     * @var array
+     */
     protected $fillable = [
         'colloquium_id',
         'theme_id'
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function colloquium()
     {
         return $this->hasMany(Colloquium::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function theme()
     {
         return $this->hasMany(Theme::class);

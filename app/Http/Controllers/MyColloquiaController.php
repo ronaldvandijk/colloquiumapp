cd73cb9a0e7fdca852283831fd0d1e252954b94e
Sander van Kasteel <info@sandervankasteel.nl>
2016-11-15 22:36:27 +0100

    Added DocBlocks for the MyColloquiaController

--- app/Http/Controllers/MyColloquiaController.php
+++ app/Http/Controllers/MyColloquiaController.php
@@ -9,19 +9,42 @@ use App\Models\Language;
 use App\Models\Theme;
 use Auth;
 
+/**
+ * Class MyColloquiaController
+ * @package App\Http\Controllers
+ */
 class MyColloquiaController
 {
 
+    /**
+     * Show an overview of all the colloquia that belongs to the current user
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         return view('user.colloquium.overview', ['colloquia' => Auth::user()->colloquia]);
     }
 
+    /**
+     * Allow the user to edit a colloquium
+     *
+     * @param ColloquiumRequest $request
+     * @param Colloquium $colloquium
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function edit(ColloquiumRequest $request, Colloquium $colloquium)
     {
         return view('user.colloquium.edit', ['colloquium' => $colloquium, 'languages' => Language::all()]);
     }
 
+    /**
+     * Update a colloquium
+     *
+     * @param ColloquiumRequest $request
+     * @param Colloquium $colloquium
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function update(ColloquiumRequest $request, Colloquium $colloquium)
     {
         $colloquium->update($request->input());
@@ -29,11 +52,22 @@ class MyColloquiaController
         return redirect(action('MyColloquiaController@index'));
     }
 
+    /**
+     * Show the user a view in which he can create a collooquium
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return view('user.colloquium.create', ['languages' => Language::all(), 'themes' => Theme::all(), 'types' => ColloquiumType::all()]);
     }
 
+    /**
+     * Store a new colloquium in the database
+     *
+     * @param RequestColloquiaRequest $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function store(RequestColloquiaRequest $request)
     {
         $colloquium = new Colloquium();

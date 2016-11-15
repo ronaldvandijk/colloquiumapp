6120b64924f29f0e762303bab75da6a555f70a9f
Sander van Kasteel <info@sandervankasteel.nl>
2016-11-16 00:35:40 +0100

    Added comments to ColloquimRequest.php

--- app/Http/Requests/ColloquiumRequest.php
+++ app/Http/Requests/ColloquiumRequest.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * ColloquiumRequest.php
+ *
+ * @author       Sander van Kasteel <info@sandervankasteel.nl>
+ */
 
 namespace App\Http\Requests;
 
 use Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
+/**
+ * Class ColloquiumRequest
+ * @package App\Http\Requests
+ */
 class ColloquiumRequest extends FormRequest
 {
     /**

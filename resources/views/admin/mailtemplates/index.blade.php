da5a0a58ccda3555fb57fda8b154eb5d11db84f1
Maarten Oosting <maarten.tutoworld@gmail.com>
2016-11-15 14:38:47 +0100

    Add new panel layout to mailtemplates

--- resources/views/admin/mailtemplates/index.blade.php
+++ resources/views/admin/mailtemplates/index.blade.php
@@ -1,65 +1,47 @@
-@extends('layouts.app')
+@extends('layouts.panel', [
+    'title' => trans('admin/mailtemplate.list_title'),
+    'btnText' => trans('admin/mailtemplate.add_template'),
+    'btnUrl' => url('/admin/mailtemplates/create'),
+])
 
-@section('title','Admin mail index')
+@section('title','Admin - ' . trans('admin/mailtemplate.list_title'))
 
 @section('content')
-    <div class="container">
-        @if(request()->session()->has('custom_error'))
-            <div class="alert alert-{{ request()->session()->get('custom_error')['type'] }}">
-                {{ request()->session()->get('custom_error')['message'] }}
-            </div>
-        @endif
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                    	@include('layouts.panel_heading', [
-                    		'title' => trans('admin/mailtemplate.list_title'),
-                    		'button' => trans('admin/mailtemplate.add_template'),
-                    		'url' => url('/admin/mailtemplates'),
-                    	])
-                    </div>
-                    <div class="panel-body">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>{{ trans('common.id') }}</th>
-                                    <th>{{ trans('admin/mailtemplate.name') }}</th>
-                                    <th>{{ trans('admin/mailtemplate.subject') }}</th>
-                                    <th>{{ trans('common.edit') }}</th>
-                                    <th>{{ trans('common.delete') }}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($data as $template)
-                                    <tr style="margin-top: 50px; margin-bottom: 5em;">
-                                        <td>{{ $template->id }}</td>
-                                        <td>{{ $template->name }}</td>
-                                        <td>{{ $template->subject }}</td>
-                                        <td>
-                                            <a href="/admin/mailtemplates/{{ $template->id }}/edit" class="btn btn-primary">
-                                                <i class="fa fa-pencil" aria-hidden="true"></i>
-                                                {{ trans('common.edit') }}
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <form method="post" action="/admin/mailtemplates/{{ $template->id }}">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button class="btn btn-danger"
-                                                        type="submit">
-                                                    <i class="fa fa-trash" aria-hidden="true"></i>
-                                                    {{ trans('common.delete') }}
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>{{ trans('common.id') }}</th>
+                <th>{{ trans('admin/mailtemplate.name') }}</th>
+                <th>{{ trans('admin/mailtemplate.subject') }}</th>
+                <th>{{ trans('common.edit') }}</th>
+                <th>{{ trans('common.delete') }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $template)
+                <tr style="margin-top: 50px; margin-bottom: 5em;">
+                    <td>{{ $template->id }}</td>
+                    <td>{{ $template->name }}</td>
+                    <td>{{ $template->subject }}</td>
+                    <td>
+                        <a href="/admin/mailtemplates/{{ $template->id }}/edit" class="btn btn-primary">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                            {{ trans('common.edit') }}
+                        </a>
+                    </td>
+                    <td>
+                        <form method="post" action="/admin/mailtemplates/{{ $template->id }}">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <button class="btn btn-danger"
+                                    type="submit">
+                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                {{ trans('common.delete') }}
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection

0d9d05f30438560c7d37300ee3debd6307a56382
ryanrio95 <ryanriohadders@gmail.com>
2016-11-15 15:19:26 +0100

    Merge branch 'master' into featureIssue84
    # Conflicts:
    #	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,29 +9,20 @@
 | contains the "web" middleware group. Now create something great!
 |
  */
-
 Auth::routes();
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/home', 'HomeController@index');
-Route::get('/test', 'TestController@overview');
-
 // TV Screen
 Route::get('/tv/{location_id?}', 'HomeController@tv');
-
 Route::group(['prefix' => 'admin', 'middleware' => 'role:administrator'], function () {
     Route::get('/', 'Admin\HomeController@index');
-
     Route::get('/profile', 'Admin\UsersController@profile');
-
     Route::get('users', 'Admin\UsersController@overview');
     Route::get('user/edit/{user}', 'Admin\UsersController@edit');
     Route::get('user/delete/{userId}', 'Admin\UsersController@delete');
     Route::post('user/update', 'Admin\UsersController@edit');
-
     Route::get('rooms', 'Admin\RoomController@index');
     Route::post('rooms', 'Admin\RoomController@store');
     Route::get('room/edit/{id}', 'Admin\RoomController@edit');
@@ -42,22 +32,19 @@ Route::group(['prefix' => 'admin', 'middleware' => 'role:administrator'], functi
     Route::resource('locations', 'Admin\LocationController');
     Route::resource('themes', 'Admin\ThemeController');
     Route::resource('buildings', 'Admin\BuildingController');
-
     Route::resource('cities', 'Admin\CityController');
-
     Route::resource('mailtemplates', 'Admin\MailtemplateController');
 });
-
-Route::group(['prefix' => 'admin/colloquia', 'middleware' => 'role:administrator|planner'], function () {
-    Route::get('/', 'Admin\ColloquiumController@index');
-    Route::get('/{status}', 'Admin\ColloquiumController@index');
-    Route::get('edit/{colloquium}', 'Admin\ColloquiumController@edit');
-    Route::post('insert', 'Admin\ColloquiumController@insert');
-    Route::post('update/{colloquium}', 'Admin\ColloquiumController@update');
-    Route::get('/approve/{colloquium}', 'Admin\ColloquiumController@approve');
-    Route::get('/deny/{colloquium}', 'Admin\ColloquiumController@deny');
+Route::group(['prefix' => 'planner', 'middleware' => 'role:administrator|planner'], function () {
+    Route::group(['prefix' => 'colloquia'], function () {
+        Route::get('/{status?}', 'Admin\ColloquiumController@index');
+        Route::get('edit/{colloquium}', 'Admin\ColloquiumController@edit');
+        Route::post('insert', 'Admin\ColloquiumController@insert');
+        Route::post('update/{colloquium}', 'Admin\ColloquiumController@update');
+        Route::get('/approve/{colloquium}', 'Admin\ColloquiumController@approve');
+        Route::get('/deny/{colloquium}', 'Admin\ColloquiumController@deny');
+    });
 });
-
 Route::group(['prefix' => 'mycolloquia', 'middleware' => 'role:user'], function () {
     Route::get('/', 'MyColloquiaController@index');
     Route::get('/edit/{colloquium}', 'MyColloquiaController@edit');
@@ -65,18 +52,17 @@ Route::group(['prefix' => 'mycolloquia', 'middleware' => 'role:user'], function 
     Route::get('/request', 'MyColloquiaController@create');
     Route::post('/store', 'MyColloquiaController@store');
 });
-
-
 Route::group(['prefix' => 'agenda'], function () {
     Route::get('/', 'AgendaController@index');
     Route::get('/show/{colloquium}', 'AgendaController@show');
 });
-
+Route::group(['prefix' => 'mailtemplates', 'middleware' => 'role:planner|administrator'], function(){
+    Route::get('/', 'MailTemplatesController@overview');
+});
 Route::group(['prefix' => 'search'], function () {
     Route::post('/', 'SearchController@index');
 });
-
-Route::group(['prefix' => 'profile', 'middleware' => 'role:user|planner|administrator'], function() {
+Route::group(['prefix' => 'profile', 'middleware' => 'role:user|planner|administrator'], function () {
     Route::get('/', 'ProfileController@index');
     Route::get('/settings', 'ProfileController@settings');
     Route::post('/settings', 'ProfileController@save');

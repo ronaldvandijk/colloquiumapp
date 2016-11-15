b155921560ab3882f9f5d78784d32f0aec3e844c
Sander van Kasteel <info@sandervankasteel.nl>
2016-11-15 23:32:32 +0100

    Added comments to BaseModel

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * BaseModel
+ * Should be extended at all times
+ *
+ * @author       Sander van Kasteel <info@sandervankasteel.nl>
+ */
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class BaseModel
+ * @package App\Models
+ */
 class BaseModel extends Model
 {
     //

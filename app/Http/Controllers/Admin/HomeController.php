8e2c6ac9bf81e57f087acf5469571be6014ae41e
Sander van Kasteel <info@sandervankasteel.nl>
2016-11-16 00:29:22 +0100

    Added comments to Admin\HomeController

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,11 +1,25 @@
 <?php
+/**
+ * HomeController
+ *
+ * @author       Sander van Kasteel
+ */
 namespace app\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 
+/**
+ * Class HomeController
+ * @package app\Http\Controllers\Admin
+ */
 class HomeController extends Controller
 {
 
+    /**
+     * Show a dashboard view to the admin user
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         return view('admin/dashboard');

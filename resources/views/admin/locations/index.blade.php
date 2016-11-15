eeced751bda8f8a686b8386c60513fd5ef75cfe9
Maarten Oosting <maarten.tutoworld@gmail.com>
2016-11-15 14:26:53 +0100

    Add new panel layout to locations

--- resources/views/admin/locations/index.blade.php
+++ resources/views/admin/locations/index.blade.php
@@ -1,63 +1,42 @@
-@extends('layouts.app')
+@extends('layouts.panel',[
+    'title' => trans('admin/location.list_title'),
+    'btnUrl' => url('/admin/locations/create'),
+    'btnText' => '<i></i> ' . trans('admin/location.create_title')
+])
 
-@section('title','Admin location index')
+@section('title','Admin - ' . trans('admin/location.list_title'))
 
-@section('content')
-    <div class="container">
-        @if(request()->session()->has('success'))
-            <div class="alert alert-success">
-             {{ request()->session()->get('success') }}
-            </div>
-        @endif
-        @if(request()->session()->has('remove'))
-            <div class="alert alert-danger">
-                {{ request()->session()->get('remove') }}
-            </div>
-        @endif
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading"><b>{{ trans('admin/location.list_title') }}</b></div>
-                    <div><a class="btn btn-default" href="{{ url('/admin/locations/create') }}">Add location</a></div>
-
-                    <div class="panel-body">
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <th>{{ trans('admin/location.id') }}</th>
-                                <th>{{ trans('admin/location.name') }}</th>
-                                <th>{{ trans('admin/location.city') }}</th>
-                                <th>{{ trans('admin/location.buildings_amount') }}</th>
-                                <th>{{ trans('admin/general.edit') }}</th>
-                                <th>{{ trans('admin/general.delete') }}</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($locations as $location)
-                                <tr style="margin-top: 50px; margin-bottom: 5em;">
-                                    <td>{{ $location->id }}</td>
-                                    <td>{{ $location->name }}</td>
-                                    <td>{{ $location->city->name }}</td>
-                                    <td><a href="{{ route("buildings.index") }}">{{ $location->buildings->count() }}</a></td>
-                                    <td><a href="/admin/locations/{{ $location->id }}/edit" class="btn btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i> {{ trans('admin/general.edit') }}</a></td>
-                                    <td>
-                                        <form method="post" action="/admin/locations/{{ $location->id }}">
-                                            {{ method_field('DELETE') }}
-                                            {{ csrf_field() }}
-                                            <button class="btn btn-danger"
-                                                    type="submit">
-                                                <i class="fa fa-trash" aria-hidden="true"></i>
-                                                {{ trans('admin/general.delete') }}
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+@section('panel-body')
+    <table class="table">
+        <thead>
+        <tr>
+            <th>{{ trans('admin/location.name') }}</th>
+            <th>{{ trans('admin/location.city') }}</th>
+            <th>{{ trans('admin/location.buildings_amount') }}</th>
+            <th style="width: 1%;">{{ trans('admin/general.edit') }}</th>
+            <th style="width: 1%;">{{ trans('admin/general.delete') }}</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($locations as $location)
+            <tr style="margin-top: 50px; margin-bottom: 5em;">
+                <td>{{ $location->name }}</td>
+                <td>{{ $location->city->name }}</td>
+                <td><a href="{{ route("buildings.index") }}">{{ $location->buildings->count() }}</a></td>
+                <td><a href="/admin/locations/{{ $location->id }}/edit" class="btn btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i> {{ trans('admin/general.edit') }}</a></td>
+                <td>
+                    <form method="post" action="/admin/locations/{{ $location->id }}">
+                        {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
+                        <button class="btn btn-danger"
+                                type="submit">
+                            <i class="fa fa-trash" aria-hidden="true"></i>
+                            {{ trans('admin/general.delete') }}
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection

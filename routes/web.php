afea694d3120a65a503252acc09b017eeb4d59d3
Sander van Kasteel <sander@hotelchamp.com>
2016-11-16 10:19:19 +0100

    Made error with resolving merge conflict

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,9 @@
 |
  */
 Auth::routes();
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', 'HomeController@index');
 
 Route::get('/', 'AgendaController@index');
+
 // TV Screen
 Route::get('/tv/{location_id?}', 'HomeController@tv');
 

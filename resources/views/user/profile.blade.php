595c0f1fd21f393be2b54ce254867154bfb79c08
TimRietdijk <t.rietdijk@st.hanze.nl>
2016-11-16 12:32:47 +0100

    bootstrap css changes

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="container-fluid">  
         <div class="row">
+        <div class="col-md-2">
             @include('user/profileSidebar')
+        </div>
             <div class="col-md-10">
                 <div class="panel panel-default">
                     <div class="panel-heading">
